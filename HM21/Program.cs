7f7ad051576f3317de44c28238d1b0ed8e5078b3
Илья Курбаков <kurbakovila@gmail.com>
2021-04-29 01:31:11 +0300

    добавлены порты по которым сервер должен ожидать получения запросов

--- HM21/Program.cs
+++ HM21/Program.cs
@@ -21,6 +21,7 @@ namespace HM21
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:5000;https://*:5002");
                 });
     }
 }
